dce222dd606e733e1a6c0355f79b3ea60e5481b9
Nandan <nk1399a@gmail.com>
2021-05-04 12:31:43 +0530

    branch 1 commit

--- app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
+++ app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+
+public class MainActivity extends AppCompatActivity {
+    //FirebaseDatabase  database;
+    //DatabaseReference dbRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //database= FirebaseDatabase.getInstance();
+       // dbRef=database.getReference("Student");
+
+        FirebaseDatabase.getInstance().getReference().child("Teachers").child("Com_Science").push().setValue("Dr. Mema");
+
+       // dbRef.setValue("Nandan");
+
+
+             print("Hello world");
+
     }
 }
