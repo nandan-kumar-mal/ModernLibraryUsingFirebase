de550cde82dcbc956875eab2773c3f8e708a745d
PriyogopalSingha <priyogopal27@gmail.com>
2021-05-11 18:43:39 +0530

    Main Menu Changes (PriCode)

--- app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
+++ app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
@@ -1,14 +1,21 @@
 package com.nandan.modernlibraryusingfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
