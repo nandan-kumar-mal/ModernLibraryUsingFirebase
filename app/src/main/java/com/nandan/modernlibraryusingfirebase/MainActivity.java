1692cc501690464dc8be8bb1bcac3b927146a834
Nk1399 <nk1399a@gmail.com>
2021-05-13 17:20:43 +0530

    Navigation Drawer is updated and cards are navigated

--- app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
+++ app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
@@ -1,22 +1,27 @@
 package com.nandan.modernlibraryusingfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
-     Toolbar toolbar;
-     NavigationView naviView;
-     DrawerLayout drawer;
-
+     private Toolbar toolbar;
+     private NavigationView naviView;
+     private DrawerLayout drawer;
+     private CardView cardTrans, cardAbout, cardAdd, cardBooklist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +30,18 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         naviView = findViewById(R.id.nav_view);
         drawer = findViewById(R.id.drawer_layout);
-
+        cardTrans = findViewById(R.id.cardTrans);
+        cardBooklist = findViewById(R.id.cardBooklist);
+        cardAdd = findViewById(R.id.cardAdd);
+        cardAbout = findViewById(R.id.cardAbout);
+        cardTrans.setOnClickListener(this);
+        cardAbout.setOnClickListener(this);
+        cardAdd.setOnClickListener(this);
+        cardBooklist.setOnClickListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        naviView.setNavigationItemSelectedListener(this);
 
 
 
@@ -42,4 +55,39 @@ public class MainActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent i;
+        switch(v.getId()){
+            case R.id.cardTrans:
+                i = new Intent(this, TransactionsActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardBooklist:
+                i = new Intent(this, Booklist.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardAdd:
+                i = new Intent(this, BookAdd.class);
+                startActivity(i);
+                break;
+
+            case R.id.cardAbout:
+                i = new Intent(this, About_us.class);
+                startActivity(i);
+                break;
+
+        }
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
 }
