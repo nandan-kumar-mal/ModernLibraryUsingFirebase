7a1ff6451f2a3c8ea64c3c3ec7470586f3408c18
Nk1399 <nk1399a@gmail.com>
2021-05-13 11:47:24 +0530

    Navigation Drawer added

--- app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
+++ app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
@@ -1,21 +1,45 @@
 package com.nandan.modernlibraryusingfirebase;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+     Toolbar toolbar;
+     NavigationView naviView;
+     DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        naviView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
 
     }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+        super.onBackPressed();
+    }
+}
 }
