c90822a7fb5b792ed3dfc120b417360a523d3c71
Nk1399 <nk1399a@gmail.com>
2021-05-21 10:19:10 +0530

    Navigation Drawer is updated and cards are navigated

--- app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
+++ app/src/main/java/com/nandan/modernlibraryusingfirebase/MainActivity.java
@@ -42,10 +42,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         naviView.setNavigationItemSelectedListener(this);
+        toggle.setToolbarNavigationClickListener(this);
+
 
 
 
     }
+    
 
     @Override
     public void onBackPressed() {
