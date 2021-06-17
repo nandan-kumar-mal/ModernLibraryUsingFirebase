1a14c18b099ef475e35fbd00175993f18f521c0a
Nk1399 <nk1399a@gmail.com>
2021-06-17 10:21:54 +0530

    New generator is added.

--- app/src/main/java/com/nandan/modernlibraryusingfirebase/BookAdd.java
+++ app/src/main/java/com/nandan/modernlibraryusingfirebase/BookAdd.java
@@ -2,14 +2,85 @@ package com.nandan.modernlibraryusingfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class BookAdd extends AppCompatActivity {
+
+    // variables for imageview, edittext,
+    // button, bitmap and qrencoder.
+    private ImageView qrCodeIV;
+    private EditText dataEdt;
+    private Button generateQrBtn, addBookBtn;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_add);
 
+        qrCodeIV = findViewById(R.id.idIVQrcode);
+        dataEdt = findViewById(R.id.idEdt);
+        generateQrBtn = findViewById(R.id.idBtnGenerateQR);
+        addBookBtn = findViewById(R.id.idBtnAddBook);
+
+        generateQrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(dataEdt.getText().toString())){
+                    Toast.makeText(BookAdd.this, "Enter some text to generate QR Code", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+                    Display display = manager.getDefaultDisplay();
+
+                    Point point = new Point();
+                    display.getSize(point);
+
+                    int width = point.x;
+                    int height = point.y;
+
+                    int dimen = width < height ? width : height;
+                    dimen  = dimen * 3/4;
+
+                    qrgEncoder = new QRGEncoder(dataEdt.getText().toString(),null, QRGContents.Type.TEXT, dimen);
+                    try{
+                        bitmap = qrgEncoder.encodeAsBitmap();
+                        qrCodeIV.setImageBitmap(bitmap);
+
+                    } catch (WriterException e) {
+                        Log.e("Tag", e.toString());
+                    }
+
+
+                }
+            }
+        });
+        addBookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BookAdd.this, BooksDetailsAdd.class);
+                intent.putExtra("title", dataEdt.getText().toString());
+
+                startActivity(intent);
+            }
+        });
     }
 }
