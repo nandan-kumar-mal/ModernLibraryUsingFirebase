d22824312cc9ae805f095b6332266e37c35af43b
Nk1399 <nk1399a@gmail.com>
2021-07-23 17:41:25 +0530

    23th july

--- app/src/main/java/com/nandan/modernlibraryusingfirebase/BookAdd.java
+++ app/src/main/java/com/nandan/modernlibraryusingfirebase/BookAdd.java
@@ -1,11 +1,15 @@
 package com.nandan.modernlibraryusingfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Display;
@@ -18,6 +22,9 @@ import android.widget.Toast;
 
 import com.google.zxing.WriterException;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
@@ -27,7 +34,7 @@ public class BookAdd extends AppCompatActivity {
     // button, bitmap and qrencoder.
     private ImageView qrCodeIV;
     private EditText dataEdt;
-    private Button generateQrBtn, addBookBtn;
+    private Button generateQrBtn, addBookBtn, btnSave;
     Bitmap bitmap;
     QRGEncoder qrgEncoder;
 
@@ -40,6 +47,9 @@ public class BookAdd extends AppCompatActivity {
         dataEdt = findViewById(R.id.idEdt);
         generateQrBtn = findViewById(R.id.idBtnGenerateQR);
         addBookBtn = findViewById(R.id.idBtnAddBook);
+        btnSave = findViewById(R.id.btnSave);
+        ActivityCompat.requestPermissions(BookAdd.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        ActivityCompat.requestPermissions(BookAdd.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
 
         generateQrBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,5 +92,52 @@ public class BookAdd extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                savetoGallery();
+
+            }
+        });
     }
+
+    private void savetoGallery() {
+
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) qrCodeIV.getDrawable();
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+
+        FileOutputStream outputStream = null;
+        File file = Environment.getExternalStorageDirectory();
+        File dir = new File(file.getAbsolutePath() + "/QR images");
+        dir.mkdir();
+
+        String filename = String.format("%d.png",System.currentTimeMillis());
+        File outfile = new File(dir, filename);
+        try{
+           outputStream = new FileOutputStream(outfile);
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        try {
+            outputStream.flush();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            outputStream.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+
+    }
+
+
 }
